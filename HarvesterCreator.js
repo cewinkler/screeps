d6a0fb797056b8a50d87039479f6d00d9c2306b9
Carl <carl@carlossus.com>
2014-12-03 19:37:44 +0800

    Added HarvesterCreator to project

---
+++ HarvesterCreator.js
@@ -0,0 +1,5 @@
+var HarvesterCreator = new function() {
+	var self = this;
+	self.role = 'builder';
+	self.body = [Game.WORK, Game.CARRY, Game.MOVE];
+};
--- Seik.js
+++ Seik.js
@@ -1,13 +1,13 @@
 function () {
 	return {
-		createCreep: function(role, creep) {
-			var likeCreeps = _.filter(Game.creeps, { memory: {role: role } });
+		createCreep: function(creep) {
+			var likeCreeps = _.filter(Game.creeps, { memory: {role: creep.role } });
 			var name = role + likeCreeps.length;
 			var result = Game.spawns.Spawn1.createCreep(creep.body, name);
 			if (result < 0) {
 				console.log("Failed to create creep, Error " + result);
 			}
-			Game.creeps[name].memory.role = role;
+			Game.creeps[name].memory.role = creep.role;
 			console.log("Successfully created creep '" + name + "'");
 		}
 	};
